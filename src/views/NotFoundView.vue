e674e505d01b6cb4c695586b906717c9fbdf8db8
IgorMMichels <igor.marcon.michels@gmail.com>
2026-07-16 22:39:12 -0300

    fix: corrige o link do botão "Voltar para Página inicial" no componente NotFoundView

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -11,7 +11,7 @@
       <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-[#E5A91A] sm:text-7xl">Café Não Encontrado</h1>
       <p class="mt-6 text-lg text-pretty text-[#3E2723] font-bold sm:text-xl/8">Parece que seu sabor de café não se encontra por aqui...</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <a href="#" class="rounded-md bg-[#3E2723] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#5e3b35]focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#5e3b35]">Voltar para Página inicial <span aria-hidden="true">&rarr;</span></a>
+        <a href="/" class="rounded-md bg-[#3E2723] px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-[#5e3b35]focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#5e3b35]">Voltar para Página inicial <span aria-hidden="true">&rarr;</span></a>
       </div>
     </div>
   </main>
